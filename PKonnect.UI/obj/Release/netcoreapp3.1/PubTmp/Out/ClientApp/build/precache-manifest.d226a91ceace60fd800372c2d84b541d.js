8f82c19bea19bf38d4e75dbba4f9687d3160b1b6
hgattu <hemanthgattu30@gmail.com>
2020-03-18 16:15:45 -0500

    Adding all remaining folders

---
+++ PKonnect.UI/obj/Release/netcoreapp3.1/PubTmp/Out/ClientApp/build/precache-manifest.d226a91ceace60fd800372c2d84b541d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f3e52f0380601721b060bae91fe72f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f0ce1117ce35e80487d",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1f203544c81468aea866",
+    "url": "/static/css/main.6ddd71b9.chunk.css"
+  },
+  {
+    "revision": "7f0ce1117ce35e80487d",
+    "url": "/static/js/2.c4b4330e.chunk.js"
+  },
+  {
+    "revision": "1f203544c81468aea866",
+    "url": "/static/js/main.ac179359.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f96c95a2e74e2c1460a1de8de1af78ce",
+    "url": "/static/media/Communities_WelcomeScreen.f96c95a2.png"
+  }
+]);
