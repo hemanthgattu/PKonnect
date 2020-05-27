bae6141cb3b0e14c980c67cf00b225b0f8ffed3e
skoppisetti <srikar.tejasri@gmail.com>
2020-05-27 11:25:24 -0700

    Fix footer and scroll up button

--- PKonnect/PKonnect.UI/ClientApp/src/environments/environment.prod.ts
+++ PKonnect/PKonnect.UI/ClientApp/src/environments/environment.prod.ts
@@ -1,16 +1,9 @@
 export const environment = {
   production: true,
-  // employeeApi: 'https://pkwebapi.azurewebsites.net/api/Employees/GetEmployeeDetails',
-  // On Prem Configs
   communitiesApi: 'https://communities.pkglobal.com/API/api',
   redirectUri: 'https://communities.pkglobal.com/',
   tenant: '06b63d62-9374-4d41-a269-4971ff69c4c7',
   clientId: 'ae57e1a3-3061-40ce-88ef-6d5c9d385160',
   amplitudeKey: 'a7e92d34197475a5fc2ea2e23d8938e3',
   sendEvents: true
-  // Azure Configs
-  // communitiesApi: 'https://pkwebapi.azurewebsites.net/api',
-  // redirectUri: 'https://pkonnectui.azurewebsites.net/',
-  // tenant: '5786743f-b4c4-4f8e-978e-95d7ff2aef9d',
-  // clientId: '870244e3-2770-4ab1-84f3-fb91d35111e5'
 };
--- PKonnect/PKonnect.UI/ClientApp/src/environments/environment.ts
+++ PKonnect/PKonnect.UI/ClientApp/src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: false,
-  // employeeApi: 'https://pkwebapi.azurewebsites.net/api/Employees/GetEmployeeDetails',
-  // communitiesApi: 'https://pkwebapi.azurewebsites.net/api',
   communitiesApi: 'https://communities.pkglobal.com/API/api',
   redirectUri: 'http://localhost:4200/',
   tenant: '5786743f-b4c4-4f8e-978e-95d7ff2aef9d',
